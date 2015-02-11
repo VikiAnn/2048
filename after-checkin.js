87b623bc7bf5fd0b960459df1d7bc789790c7188
Greg Narajka <gregnar@gmail.com>
2015-02-11 14:17:03 -0700

    set up rows and columns arrays and rowcol objects

---
+++ after-checkin.js
@@ -0,0 +1,26 @@
+var row1col1 = {
+  occupant: tile2();
+};
+var row1col2 = {
+  occupant: null;
+};
+var row1col3 = {
+  occupant: null;
+};
+var row1col4 = {
+  occupant: null;
+};
+
+var tile2 = function() {
+  return { value: 2 };
+};
+
+var row1 =[row1col1, row1col2, row1col3, row1col4]
+var row2 =[1, 2, 3, 4]
+var row3 =[1, 2, 3, 4]
+var row4 =[1, 2, 3, 4]
+
+var col1 =[row1col1, row2col1, row3col1, row4col1]
+var col2 =[1, 2, 3, 4]
+var col3 =[1, 2, 3, 4]
+var col4 =[1, 2, 3, 4]
