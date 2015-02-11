2989d318f0fa2cfa339b44d2a1d47fb90096bf58
Greg Narajka <gregnar@gmail.com>
2015-02-11 16:54:25 -0700

    we moved a tile to the right

--- after-checkin.js
+++ after-checkin.js
@@ -1,26 +1,54 @@
+require('locus')
+var tile2 = function() {
+  return { number: 2, cssClass: "tile" };
+};
+
 var row1col1 = {
-  occupant: tile2();
+  occupant: tile2(),
 };
 var row1col2 = {
-  occupant: null;
+  occupant: null,
 };
 var row1col3 = {
-  occupant: null;
+  occupant: null,
 };
 var row1col4 = {
-  occupant: null;
+  occupant: null,
 };
 
-var tile2 = function() {
-  return { value: 2 };
-};
 
 var row1 =[row1col1, row1col2, row1col3, row1col4]
-var row2 =[1, 2, 3, 4]
-var row3 =[1, 2, 3, 4]
-var row4 =[1, 2, 3, 4]
 
-var col1 =[row1col1, row2col1, row3col1, row4col1]
-var col2 =[1, 2, 3, 4]
-var col3 =[1, 2, 3, 4]
-var col4 =[1, 2, 3, 4]
+var moveRowRight = function(row) {
+  do {
+    startingRow = row
+    var newRow = getNextRowIteration(row);
+  } while (newRow !== startingRow);
+  return newRow;
+};
+
+
+var getNextRowIteration = function(row) {
+  for (i=0; i < (row.length - 1); i++) {
+    var newRow = moveTileRightIfTileExists(row, i);
+  };
+  return row
+}
+
+
+var moveTileRightIfTileExists = function(row, index) {
+  var currentSquare = row[index];
+  var currentTile   = currentSquare.occupant;
+  var nextSquare    = row[index + 1];
+  if (!currentTile || nextSquare.occupant) {
+    return;
+  } else {
+    nextSquare.occupant    = currentTile;
+    currentSquare.occupant = null;
+  };
+  return row
+};
+
+console.log(row1)
+var finalRow = moveRowRight(row1)
+console.log(row1)
